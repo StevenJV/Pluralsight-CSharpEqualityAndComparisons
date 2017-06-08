fe19d9b9d3f38a5314aca57a97e64fe12804536f
StevenJV <StevenVore@tnwinc.com>
2017-06-08 09:58:23 -0400

    initial Food object

---
+++ Food/Food/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Food banana = new Food("banana");
+      Food chocolate = new Food("chocolate");
+
+      Console.WriteLine($"Banana.Equals(chocolate) ? {banana.Equals(chocolate)}");
+  }
+  }
+}
