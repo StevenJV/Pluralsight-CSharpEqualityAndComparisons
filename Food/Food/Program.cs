f162035b25d6808a3edff6778762c66953830956
StevenJV <StevenVore@tnwinc.com>
2017-06-08 10:35:00 -0400

    string equality

--- Food/Food/Program.cs
+++ Food/Food/Program.cs
@@ -11,9 +11,18 @@ namespace Food
     static void Main(string[] args)
     {
       Food banana = new Food("banana");
+      Food banana2 = new Food("banana");
       Food chocolate = new Food("chocolate");
 
       Console.WriteLine($"Banana.Equals(chocolate) ? {banana.Equals(chocolate)}");
-  }
+      Console.WriteLine($"Banana.Equals(banana2) ? {banana.Equals(banana2)} // false because object.Equals() asks 'is this the same instance?'");
+
+      string bananaStr = "banana";
+      string copyOfBananaStr = string.Copy(bananaStr);
+
+      Console.WriteLine($"BananaStr.Equals((object)copyOfBananaStr) ? {bananaStr.Equals((object)copyOfBananaStr)} // string.Equals looks at contnets, treating strings like values rather than references");
+
+
+    }
   }
 }
