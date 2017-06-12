c62736afd8c990c5eff95c7065bc175991b74eea
StevenJV <StevenVore@tnwinc.com>
2017-06-12 14:07:46 -0400

    nulls, referenceEquals

--- Food/Food/Program.cs
+++ Food/Food/Program.cs
@@ -16,11 +16,17 @@ namespace Food
 
       Console.WriteLine($"Banana.Equals(chocolate) ? {banana.Equals(chocolate)}");
       Console.WriteLine($"Banana.Equals(banana2) ? {banana.Equals(banana2)} // false because object.Equals() asks 'is this the same instance?'");
+      Console.WriteLine($"Banana.Equals(null) ? {banana.Equals(null)}  // null NEVER equals any non-null reference");
+      Food noBanana = null;
+      //Console.WriteLine($"noBanana.Equals(Banana) ? {noBanana.Equals(banana)}  // run-time exception");
+      Console.WriteLine($"object.Equals(banana, noBanana) ? {object.Equals(banana, noBanana)}  // null NEVER equals any non-null reference, no run-time exception"); 
+      Console.WriteLine($"object.Equals(null, noBanana) ? {object.Equals(null, noBanana)}  // null always equals null");
 
       string bananaStr = "banana";
       string copyOfBananaStr = string.Copy(bananaStr);
 
-      Console.WriteLine($"BananaStr.Equals((object)copyOfBananaStr) ? {bananaStr.Equals((object)copyOfBananaStr)} // STRUCTS are values ");
+      Console.WriteLine($"BananaStr.Equals((object)copyOfBananaStr) ? {bananaStr.Equals((object)copyOfBananaStr)} // string.Equals looks at contnets, treating strings like values rather than references ");
+      Console.WriteLine($"ReferenceEquals(bananaStr,copyOfBananaStr) ? {ReferenceEquals(bananaStr,copyOfBananaStr)} // ReferenceEquals checks for two pointers to the same object ");
 
 
     }
