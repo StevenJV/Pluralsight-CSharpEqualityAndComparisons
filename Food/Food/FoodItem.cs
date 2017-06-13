e37c4476a2dfb11b5bf30e8f8e4ec49ed7a60e04
StevenJV <StevenVore@tnwinc.com>
2017-06-13 16:10:25 -0400

    create new FoodItem struct

---
+++ Food/Food/FoodItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food
+{
+  public enum FoodGroup { Meat, Fruit, Vegetables, Sweets}
+  struct FoodItem
+  {
+    private readonly string _name;
+    private readonly FoodGroup _group;
+
+    public string Name => _name;
+    public FoodGroup Group => _group;
+
+    public FoodItem(string name, FoodGroup group)
+    {
+      this._name = name;
+      this._group = group;
+    }
+
+    public override string ToString()
+    {
+      return _name;
+    }
+
+  }
+}
