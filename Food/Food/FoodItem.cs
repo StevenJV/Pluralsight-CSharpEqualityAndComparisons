2282216e740f18a96cf8d968a1bf79101dfceced
StevenJV <StevenVore@tnwinc.com>
2017-06-14 09:44:35 -0400

    implement IEquatable interface

--- Food/Food/FoodItem.cs
+++ Food/Food/FoodItem.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Food
 {
   public enum FoodGroup { Meat, Fruit, Vegetables, Sweets}
-  struct FoodItem
+  struct FoodItem : IEquatable<FoodItem>
   {
     private readonly string _name;
     private readonly FoodGroup _group;
@@ -19,6 +19,11 @@ namespace Food
     {
       this._name = name;
       this._group = group;
+    }
+
+    public bool Equals(FoodItem other)
+    {
+      return this._name == other._name && this._group == other._group;
     }
 
     public override string ToString()
