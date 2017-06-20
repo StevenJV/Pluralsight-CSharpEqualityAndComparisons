c01928b455a74f61747e37e5d81bb6504879261e
StevenJV <StevenVore@tnwinc.com>
2017-06-20 10:49:42 -0400

    another small cleanup

--- Food/Food/Program.cs
+++ Food/Food/Program.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Food
 {
   class Program
   {
-    static void Main(string[] args)
-    {
+    static void Main(string[] args){
       Food banana = new Food("banana", FoodGroup.Fruit);
       Food banana2 = new Food("banana", FoodGroup.Fruit);
       Food chocolate = new Food("chocolate", FoodGroup.Sweets);
@@ -33,7 +28,7 @@ namespace Food
         $"ReferenceEquals(bananaStr,copyOfBananaStr) ? {ReferenceEquals(bananaStr, copyOfBananaStr)} // ReferenceEquals checks for two pointers to the same object ");
 
 
-      Console.WriteLine("\n--- overloaded Equals() and == for Value Types");
+      Console.WriteLine("\n--- overloaded Equals() and == for Value Types. Structs are value types.");
 
       FoodItem redDelicious = new FoodItem("apple", FoodGroup.Fruit);
       FoodItem grannySmith = new FoodItem("apple", FoodGroup.Fruit);
@@ -47,7 +42,7 @@ namespace Food
       Console.WriteLine("grannySmith.Equals(cake)        : " + grannySmith.Equals(cake));
       Console.WriteLine("cake.Equals(redDelicious)       : " + cake.Equals(redDelicious));
 
-      Console.WriteLine("\n--- overloaded Equals() and == for Reference Types");
+      Console.WriteLine("\n--- overloaded Equals() and == for Reference Types. , Classes are reference types.");
       Food apple = new Food("apple", FoodGroup.Fruit);
       CookedFood stewedApple = new CookedFood("stewed", "apple", FoodGroup.Fruit);
       CookedFood bakedApple = new CookedFood("baked", "apple", FoodGroup.Fruit);
@@ -61,8 +56,7 @@ namespace Food
       DisplayWhetherEqueal(apple, apple);
     }
 
-    private static void DisplayWhetherEqueal(Food food1, Food food2)
-    {
+    private static void DisplayWhetherEqueal(Food food1, Food food2){
       Console.WriteLine(food1 == food2 ? $"{food1,12} == {food2}" : $"{food1,12} != {food2}");
     }
   }
